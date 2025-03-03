e4e5cf86f52b9a930993f4b7c1582442d5edccf9
David Dubbeldam <david.dubbeldam@kpnmail.nl>
2025-03-03 15:05:34 +0100

    MNT: Re-rendered with conda-build 25.1.2, conda-smithy 3.46.1, and conda-forge-pinning 2025.03.03.02.00.54

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+
+if [[ "$target_platform" == linux-* ]]; then
+  # workaround a binutils bug
+  export LDFLAGS="${LDFLAGS} -Wl,-rpath-link,$(pwd)/lib"
+  export LDFLAGS=$(echo "${LDFLAGS}" | sed "s/-Wl,--as-needed//g")
+fi
+
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:-}" != "" ]]; then
   cmake -B build --preset conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX}
 else
   cmake ${CMAKE_ARGS} -B build --preset conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_OSX_ARCHITECTURES="arm64"
 fi
+
 ninja -C build install -v
