b9c7ba827f2295ba720ab36739994988993ccf7d
David Dubbeldam <david.dubbeldam@kpnmail.nl>
2025-03-03 15:25:42 +0100

    MNT: Re-rendered with conda-build 25.1.2, conda-smithy 3.46.1, and conda-forge-pinning 2025.03.03.02.00.54

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,7 +9,12 @@ fi
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:-}" != "" ]]; then
   cmake -B build --preset conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX}
 else
-  cmake ${CMAKE_ARGS} -B build --preset conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_OSX_ARCHITECTURES="arm64"
+  if [[ "$target_platform" == linux-* ]]; then
+    cmake ${CMAKE_ARGS} -B build --preset conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_SYSTEM_PROCESSOR="aarch64"
+  fi
+  if [[ "$target_platform" == osx-* ]]; then
+    cmake ${CMAKE_ARGS} -B build --preset conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_OSX_ARCHITECTURES="arm64"
+  fi
 fi
 
 ninja -C build install -v
