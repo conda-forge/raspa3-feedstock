f55ef1980b9f5785122bd2e6b5fd083dbd2ecf68
David Dubbeldam <david.dubbeldam@kpnmail.nl>
2025-03-05 19:55:24 +0100

    try aarch64 support

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:-}" != "" ]]; then
-  cmake -B build --preset conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBUILD_TESTING=OFF
+  cmake -B build --preset conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBUILD_TESTING=OFF -DBLA_SIZEOF_INTEGER="8"
 else
-  cmake ${CMAKE_ARGS} -B build --preset conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_OSX_ARCHITECTURES="arm64" -DBUILD_TESTING=OFF
+  if [[ "${target_platform}" == "linux"* ]]; then
+    cmake ${CMAKE_ARGS} -B build --preset conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBLA_SIZEOF_INTEGER="8" -DCMAKE_CXX_FLAGS_RELEASE="-march=armv8-a ${CMAKE_CXX_FLAGS_RELEASE}" -DBUILD_TESTING=OFF
+  fi
+  if [[ "${target_platform}" == osx-* ]]; then
+    cmake ${CMAKE_ARGS} -B build --preset conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBLA_SIZEOF_INTEGER="8" -DCMAKE_OSX_ARCHITECTURES="arm64" -DBUILD_TESTING=OFF
+  fi
 fi
 ninja -C build install -v
