e652d9bdc8eac765d32dce77b8892c8ee49417c3
David Dubbeldam <david.dubbeldam@kpnmail.nl>
2025-03-05 20:06:58 +0100

    remove 64-bits lapack requirement

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,10 +3,10 @@ if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:
   cmake -B build --preset conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBUILD_TESTING=OFF -DBLA_SIZEOF_INTEGER="8"
 else
   if [[ "${target_platform}" == "linux"* ]]; then
-    cmake ${CMAKE_ARGS} -B build --preset conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBLA_SIZEOF_INTEGER="8" -DCMAKE_CXX_FLAGS_RELEASE="-march=armv8-a ${CMAKE_CXX_FLAGS_RELEASE}" -DBUILD_TESTING=OFF
+    cmake ${CMAKE_ARGS} -B build --preset conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_CXX_FLAGS_RELEASE="-march=armv8-a ${CMAKE_CXX_FLAGS_RELEASE}" -DBUILD_TESTING=OFF
   fi
   if [[ "${target_platform}" == osx-* ]]; then
-    cmake ${CMAKE_ARGS} -B build --preset conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBLA_SIZEOF_INTEGER="8" -DCMAKE_OSX_ARCHITECTURES="arm64" -DBUILD_TESTING=OFF
+    cmake ${CMAKE_ARGS} -B build --preset conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_OSX_ARCHITECTURES="arm64" -DBUILD_TESTING=OFF
   fi
 fi
 ninja -C build install -v
