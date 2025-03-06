17f97ab17f1e5c72220fcfc32a3aa6290fe7234f
David Dubbeldam <david.dubbeldam@kpnmail.nl>
2025-03-06 10:07:24 +0100

    MNT: Re-rendered with conda-build 25.1.2, conda-smithy 3.46.1, and conda-forge-pinning 2025.03.05.16.19.52

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,12 @@ if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:
   cmake -B build --preset conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBUILD_TESTING=OFF
 else
   if [[ "${target_platform}" == "linux"* ]]; then
-    cmake ${CMAKE_ARGS} -B build --preset conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBUILD_TESTING=OFF -DDBLAS_ROOT=${PREFIX}/lib
+    export OPENBLAS_USES_OPENMP=1
+    ldd $PREFIX/lib/libopenblas.so
+    ldd $PREFIX/lib/libblas.so
+    ls -lagh $PREFIX/lib/*blas*
+    ls -lagh $PREFIX/lib/*lapack*
+    cmake ${CMAKE_ARGS} -B build --preset conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBUILD_TESTING=OFF
   fi
   if [[ "${target_platform}" == osx-* ]]; then
     cmake ${CMAKE_ARGS} -B build --preset conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_OSX_ARCHITECTURES="arm64" -DBUILD_TESTING=OFF
