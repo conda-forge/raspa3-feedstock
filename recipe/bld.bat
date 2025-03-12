399fdea270b1d8816a5ca28c637317419c2bf927
David Dubbeldam <david.dubbeldam@kpnmail.nl>
2025-03-12 21:47:48 +0100

    MNT: Re-rendered with conda-build 25.1.2, conda-smithy 3.47.0, and conda-forge-pinning 2025.03.12.19.21.36

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,4 @@
-cmake -B build -G "Ninja Multi-Config" --preset windows_conda_raspa3 -DCMAKE_INSTALL_PREFIX=%PREFIX% 
+cmake ${CMAKE_ARGS} -B build -G "Ninja Multi-Config" --preset windows_conda_raspa3 -DCMAKE_INSTALL_PREFIX=%PREFIX% 
 if errorlevel 1 exit 1
 ninja -C build install
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:-}" != "" ]]; then
   if [[ "${target_platform}" == linux-64 ]]; then
-    cmake -B build --preset linux_conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBUILD_TESTING=OFF
+    cmake -B build --preset linux_conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} 
   elif [[ "${target_platform}" == linux-ppc64le ]]; then
-    cmake -B build --preset linux_conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBUILD_TESTING=OFF -DFINAL_CXXFLAGS="--target=ppc64le ${FINAL_CXXFLAGS}"
+    cmake -B build --preset linux_conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBUILD_TESTING=OFF 
   elif [[ "${target_platform}" == linux-aarch64 ]]; then
-    cmake -B build --preset linux_conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBUILD_TESTING=OFF -DFINAL_CXXFLAGS="--target=aarch64 ${FINAL_CXXFLAGS}"
+    cmake -B build --preset linux_conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBUILD_TESTING=OFF 
   elif [[ "${target_platform}" == osx-* ]]; then
-    cmake ${CMAKE_ARGS} -B build --preset mac_conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBUILD_TESTING=OFF
+    cmake ${CMAKE_ARGS} -B build --preset mac_conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX}
   fi
 else
   if [[ "${target_platform}" == "linux"* ]]; then
-    cmake -B build --preset linux_conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBUILD_TESTING=OFF
+    cmake -B build --preset linux_conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX}
   elif  [[ "${target_platform}" == osx-* ]]; then
-    cmake ${CMAKE_ARGS} -B build --preset mac_conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBUILD_TESTING=OFF -DCMAKE_OSX_ARCHITECTURES="arm64"
+    cmake ${CMAKE_ARGS} -B build --preset mac_conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_OSX_ARCHITECTURES="arm64"
   fi
 fi
 ninja -C build install -v
