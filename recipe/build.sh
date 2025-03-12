202d6e768bbb5f36edc9bd1f9015f494da3286a6
David Dubbeldam <david.dubbeldam@kpnmail.nl>
2025-03-12 15:52:23 +0100

    attempt windows and linux arm support

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:-}" != "" ]]; then
-  cmake -B build --preset conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBUILD_TESTING=OFF
+  if [[ "${target_platform}" == "linux"* ]]; then
+    cmake -B build --preset linux_conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBUILD_TESTING=OFF
+  elif [[ "${target_platform}" == osx-* ]]; then
+    cmake -B build --preset mac_conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBUILD_TESTING=OFF
+  fi
 else
   if [[ "${target_platform}" == "linux"* ]]; then
-    cmake ${CMAKE_ARGS} -B build --preset conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBUILD_TESTING=OFF -DOpenBLAS_INCLUDE_DIRS=$PREFIX/include  -DOpenBLAS_LIBRARIES=$PREFIX/lib/libopenblas.0.so
-  fi
-  if [[ "${target_platform}" == osx-* ]]; then
-    cmake ${CMAKE_ARGS} -B build --preset conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_OSX_ARCHITECTURES="arm64" -DBUILD_TESTING=OFF
+    cmake -B build --preset linux_conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBUILD_TESTING=OFF
+  elif  [[ "${target_platform}" == osx-* ]]; then
+    cmake -B build --preset mac_conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBUILD_TESTING=OFF -DCMAKE_OSX_ARCHITECTURES="arm64"
   fi
 fi
 ninja -C build install -v
