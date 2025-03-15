368f63b2453af0f1b359607d3c469b4ae4b10c1a
David Dubbeldam <david.dubbeldam@kpnmail.nl>
2025-03-15 10:38:09 +0100

    attempt cross-compilation

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:-}" != "" ]]; then
-  if [[ "${target_platform}" == linux-64 ]]; then
-    cmake -B build --preset linux_conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} 
-  elif [[ "${target_platform}" == linux-ppc64le ]]; then
-    cmake -B build --preset linux_conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBUILD_TESTING=OFF
-  elif [[ "${target_platform}" == linux-aarch64 ]]; then
-    cmake -B build --preset linux_conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBUILD_TESTING=OFF 
+
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
+  if [[ "${target_platform}" == linux* ]]; then
+    cmake -B build --preset=linux_conda_raspa3 
   elif [[ "${target_platform}" == osx-* ]]; then
-    cmake ${CMAKE_ARGS} -B build --preset mac_conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX}
+    cmake -B build --preset=mac_conda_raspa3
   fi
 else
-  if [[ "${target_platform}" == "linux"* ]]; then
-    cmake -B build --preset linux_conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX}
+  if [[ "${target_platform}" == linux* ]]; then
+    cmake -B build ${CMAKE_ARGS} --preset=linux_conda_raspa3
   elif  [[ "${target_platform}" == osx-* ]]; then
-    cmake ${CMAKE_ARGS} -B build --preset mac_conda_raspa3 -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_OSX_ARCHITECTURES="arm64"
+    cmake -B build ${CMAKE_ARGS} --preset=mac_conda_raspa3
   fi
 fi
+
 ninja -C build install -v
