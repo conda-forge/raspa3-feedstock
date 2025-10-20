3db801e13273ef7f3a457eadaaad975f1f8d2339
David Dubbeldam <d.dubbeldam@uva.nl>
2025-10-20 11:39:37 +0200

    MNT: Re-rendered with conda-smithy 3.52.3 and conda-forge-pinning 2025.10.18.16.08.03
    Other tools:
    - conda-build 25.9.0
    - rattler-build 0.48.1
    - rattler-build-conda-compat 1.4.9

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,17 +2,17 @@
 
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
   if [[ "${target_platform}" == linux* ]]; then
-    cmake -B build --preset=linux_conda_raspa3 -DCMAKE_POLICY_VERSION_MINIMUM=3.32 -DCMAKE_C_COMPILER=${CC_FOR_BUILD} -DCMAKE_CXX_COMPILER=${CXX_FOR_BUILD}
+    cmake -B build --preset=linux_conda_raspa3 -DCMAKE_POLICY_VERSION_MINIMUM=3.32 -DCMAKE_C_COMPILER="$BUILD_PREFIX/bin/x86_64-conda-linux-gnu-gcc" -DCMAKE_CXX_COMPILER="$BUILD_PREFIX/bin/x86_64-conda-linux-gnu-g++" -DCMAKE_CXX_COMPILER_AR="$BUILD_PREFIX/bin/x86_64-conda-linux-gnu-ar" -DCMAKE_CXX_COMPILER_RANLIB="$BUILD_PREFIX/bin/x86_64-conda-linux-gnu-ranlib" -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=false
   elif [[ "${target_platform}" == osx-* ]]; then
-    cmake -B build --preset=mac_conda_raspa3 -DCMAKE_POLICY_VERSION_MINIMUM=3.32 -DCMAKE_C_COMPILER=${CC_FOR_BUILD} -DCMAKE_CXX_COMPILER=${CXX_FOR_BUILD}
+    cmake -B build --preset=mac_conda_raspa3 -DCMAKE_POLICY_VERSION_MINIMUM=3.32
   fi
 else
   if [[ "${target_platform}" == linux-aarch64 ]]; then 
-    cmake -B build --preset=linux_conda_raspa3 ${CMAKE_ARGS}  -DCMAKE_CXX_COMPILER_AR="$BUILD_PREFIX/bin/aarch64-conda-linux-gnu-ar" -DCMAKE_CXX_COMPILER_RANLIB="$BUILD_PREFIX/bin/aarch64-conda-linux-gnu-ranlib" -DCMAKE_POLICY_VERSION_MINIMUM=3.32 -DCMAKE_C_COMPILER=${CC_FOR_BUILD} -DCMAKE_CXX_COMPILER=${CXX_FOR_BUILD}
+    cmake -B build --preset=linux_conda_raspa3 ${CMAKE_ARGS}  -DCMAKE_CXX_COMPILER_AR="$BUILD_PREFIX/bin/aarch64-conda-linux-gnu-ar" -DCMAKE_CXX_COMPILER_RANLIB="$BUILD_PREFIX/bin/aarch64-conda-linux-gnu-ranlib" -DCMAKE_POLICY_VERSION_MINIMUM=3.32 -DCMAKE_C_COMPILER="$BUILD_PREFIX/bin/aarch64-conda-linux-gnu-gcc" -DCMAKE_CXX_COMPILER="$BUILD_PREFIX/bin/aarch64-conda-linux-gnu-g++" -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=false
   elif [[ "${target_platform}" == linux-ppc64le ]]; then
-    cmake -B build --preset=linux_conda_raspa3 ${CMAKE_ARGS} -DCMAKE_CXX_COMPILER_AR="$BUILD_PREFIX/bin/powerpc64le-conda-linux-gnu-ar" -DCMAKE_CXX_COMPILER_RANLIB="$BUILD_PREFIX/bin/powerpc64le-conda-linux-gnu-ranlib" -DCMAKE_POLICY_VERSION_MINIMUM=3.32 -DCMAKE_C_COMPILER=${CC_FOR_BUILD} -DCMAKE_CXX_COMPILER=${CXX_FOR_BUILD}
+    cmake -B build --preset=linux_conda_raspa3 ${CMAKE_ARGS} -DCMAKE_CXX_COMPILER_AR="$BUILD_PREFIX/bin/powerpc64le-conda-linux-gnu-ar" -DCMAKE_CXX_COMPILER_RANLIB="$BUILD_PREFIX/bin/powerpc64le-conda-linux-gnu-ranlib" -DCMAKE_POLICY_VERSION_MINIMUM=3.32 -DCMAKE_C_COMPILER="$BUILD_PREFIX/bin/powerpc64le-conda-linux-gnu-gcc" -DCMAKE_CXX_COMPILER="$BUILD_PREFIX/bin/powerpc64le-conda-linux-gnu-g++" -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=false
   elif  [[ "${target_platform}" == osx-* ]]; then
-    cmake -B build --preset=mac_conda_raspa3 ${CMAKE_ARGS} -DCMAKE_POLICY_VERSION_MINIMUM=3.32 -DCMAKE_C_COMPILER=${CC_FOR_BUILD} -DCMAKE_CXX_COMPILER=${CXX_FOR_BUILD}
+    cmake -B build --preset=mac_conda_raspa3 ${CMAKE_ARGS} -DCMAKE_POLICY_VERSION_MINIMUM=3.32 
   fi
 fi
 
