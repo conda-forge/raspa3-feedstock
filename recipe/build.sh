36c879d65b35db9ebd3f0c58e08778847f8602b3
David Dubbeldam <d.dubbeldam@uva.nl>
2025-10-27 10:59:37 +0100

    MNT: Re-rendered with conda-smithy 3.52.3 and conda-forge-pinning 2025.10.27.09.15.48
    Other tools:
    - conda-build 25.9.0
    - rattler-build 0.48.1
    - rattler-build-conda-compat 1.4.9

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,12 +8,25 @@ if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
   fi
 else
   if [[ "${target_platform}" == linux-aarch64 ]]; then 
-    cmake -B build --preset=linux_conda_raspa3_gcc ${CMAKE_ARGS} -DCMAKE_POLICY_VERSION_MINIMUM=3.32
+    cmake -B build --preset=linux_conda_raspa3_gcc ${CMAKE_ARGS} -DCMAKE_POLICY_VERSION_MINIMUM=3.32 -DBUILD_APP=true -DBUILD_CLI=false -DBUILD_TESTING=false
   elif [[ "${target_platform}" == linux-ppc64le ]]; then
-    cmake -B build --preset=linux_conda_raspa3_gcc ${CMAKE_ARGS} -DCMAKE_POLICY_VERSION_MINIMUM=3.32
+    cmake -B build --preset=linux_conda_raspa3_gcc ${CMAKE_ARGS} -DCMAKE_POLICY_VERSION_MINIMUM=3.32 -DBUILD_APP=true -DBUILD_CLI=false -DBUILD_TESTING=false
   elif  [[ "${target_platform}" == osx-* ]]; then
     cmake -B build --preset=mac_conda_raspa3 ${CMAKE_ARGS} -DCMAKE_POLICY_VERSION_MINIMUM=3.32
   fi
 fi
 
-ninja -C build install -v -j1
+ninja -C build install -v
+
+# second stage for linux cross-compilation
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
+  if [[ "${target_platform}" == linux-aarch64 ]]; then 
+    rm -rf build
+    cmake -B build --preset=linux_conda_raspa3_gcc ${CMAKE_ARGS} -DCMAKE_POLICY_VERSION_MINIMUM=3.32 -DBUILD_APP=false -DBUILD_CLI=true -DBUILD_TESTING=false
+    ninja -C build install -v
+  elif [[ "${target_platform}" == linux-ppc64le ]]; then
+    rm -rf build
+    cmake -B build --preset=linux_conda_raspa3_gcc ${CMAKE_ARGS} -DCMAKE_POLICY_VERSION_MINIMUM=3.32 -DBUILD_APP=false -DBUILD_CLI=true -DBUILD_TESTING=false
+    ninja -C build install -v
+  fi
+fi
