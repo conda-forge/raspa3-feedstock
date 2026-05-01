6bc89783325a5c6948655d3cf3ad3de4a8492cbb
David Dubbeldam <d.dubbeldam@uva.nl>
2026-05-01 20:53:51 +0200

    chore: test cmake install

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,25 +7,25 @@ if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
   if [[ "${target_platform}" == linux* ]]; then
     CXXFLAGS=-stdlib=libstdc++ cmake -B build --preset=linux_conda_raspa3 -DCMAKE_POLICY_VERSION_MINIMUM=3.32 -DBUILD_APP=true -DBUILD_CLI=false -DBUILD_TESTING=false
     cmake --build build --config Release -v
-    ninja -C build install -v
+    cmake --install build --config Release -v
   elif [[ "${target_platform}" == osx-* ]]; then
     cmake -B build --preset=mac_conda_raspa3 -DCMAKE_POLICY_VERSION_MINIMUM=3.32
     cmake --build build --config Release -v
-    ninja -C build install -v
+    cmake --install build --config Release -v
   fi
 else
   if [[ "${target_platform}" == linux-aarch64 ]]; then 
     cmake -B build --preset=linux_conda_raspa3 ${CMAKE_ARGS} -DCMAKE_POLICY_VERSION_MINIMUM=3.32 -DBUILD_APP=true -DBUILD_CLI=false -DBUILD_TESTING=false
     cmake --build build --config Release -v
-    ninja -C build install -v
+    cmake --install build --config Release -v
   elif [[ "${target_platform}" == linux-ppc64le ]]; then
     cmake -B build --preset=linux_conda_raspa3 ${CMAKE_ARGS} -DCMAKE_POLICY_VERSION_MINIMUM=3.32 -DBUILD_APP=true -DBUILD_CLI=false -DBUILD_TESTING=false
     cmake --build build --config Release -v
-    ninja -C build install -v
+    cmake --install build --config Release -v
   elif  [[ "${target_platform}" == osx-* ]]; then
     cmake -B build --preset=mac_conda_raspa3 ${CMAKE_ARGS} -DCMAKE_POLICY_VERSION_MINIMUM=3.32
     cmake --build build --config Release -v
-    ninja -C build install -v
+    cmake --install build --config Release -v
   fi
 fi
 
@@ -36,19 +36,19 @@ if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
     rm -rf build
     cmake -B build --preset=linux_conda_raspa3 -DCMAKE_POLICY_VERSION_MINIMUM=3.32 -DBUILD_APP=false -DBUILD_CLI=true -DBUILD_TESTING=false
     cmake --build build --config Release -v
-    ninja -C build install -v
+    cmake --install build --config Release -v
   fi
 else
   if [[ "${target_platform}" == linux-aarch64 ]]; then 
     rm -rf build
     cmake -B build --preset=linux_conda_raspa3 ${CMAKE_ARGS} -DCMAKE_POLICY_VERSION_MINIMUM=3.32 -DBUILD_APP=false -DBUILD_CLI=true -DBUILD_TESTING=false
     cmake --build build --config Release -v
-    ninja -C build install -v
+    cmake --install build --config Release -v
   elif [[ "${target_platform}" == linux-ppc64le ]]; then
     rm -rf build
     cmake -B build --preset=linux_conda_raspa3 ${CMAKE_ARGS} -DCMAKE_POLICY_VERSION_MINIMUM=3.32 -DBUILD_APP=false -DBUILD_CLI=true -DBUILD_TESTING=false
     cmake --build build --config Release -v
-    ninja -C build install -v
+    cmake --install build --config Release -v
   fi
 fi
 
@@ -58,18 +58,18 @@ if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
     rm -rf build
     cmake -B build --preset=linux_conda_raspa3 -DCMAKE_POLICY_VERSION_MINIMUM=3.32 -DBUILD_APP=false -DBUILD_CLI=false -DBUILD_TESTING=true
     cmake --build build --config Release -v
-    ninja -C build install -v
+    cmake --install build --config Release -v
   fi
 else
   if [[ "${target_platform}" == linux-aarch64 ]]; then 
     rm -rf build
     cmake -B build --preset=linux_conda_raspa3 ${CMAKE_ARGS} -DCMAKE_POLICY_VERSION_MINIMUM=3.32 -DBUILD_APP=false -DBUILD_CLI=false -DBUILD_TESTING=true
     cmake --build build --config Release -v
-    ninja -C build install -v
+    cmake --install build --config Release -v
   elif [[ "${target_platform}" == linux-ppc64le ]]; then
     rm -rf build
     cmake -B build --preset=linux_conda_raspa3 ${CMAKE_ARGS} -DCMAKE_POLICY_VERSION_MINIMUM=3.32 -DBUILD_APP=false -DBUILD_CLI=false -DBUILD_TESTING=true
     cmake --build build --config Release -v
-    ninja -C build install -v
+    cmake --install build --config Release -v
   fi
 fi
